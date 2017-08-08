3ca54c88a8df3ad57f74087ab9795a4668d88a24
stanislavkisyov <stanislav.kisiov@gmail.com>
2017-08-08 13:13:55 +0300

    Max Sequence of Equal Elements

--- Java Basics - Exercises/src/E07Max_Sequence_of_Equal_Elements.java
+++ Java Basics - Exercises/src/E07Max_Sequence_of_Equal_Elements.java
@@ -1,9 +1,32 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class E07Max_Sequence_of_Equal_Elements {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        
+
+        int[] input = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int currentLen = 1;
+        int currentPos = 0;
+        int bestLen = 1;
+
+
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] == input[i - 1]) {
+                currentLen++;
+                if (currentLen > bestLen) {
+                    bestLen = currentLen;
+                    currentPos = i;
+                }
+            } else {
+                currentLen = 1;
+
+            }
+        }
+        for (int i = 0; i < bestLen; i++) {
+            System.out.print(input[currentPos] + " ");
+        }
     }
+
 }
